a42bd36d94ac0c8abcbc54c2d26d5b18b0a52b77
Maria <62722772+Prfnnk@users.noreply.github.com>
2020-05-30 14:33:48 +0300

    add files

---
+++ precache-manifest.5b5140947dd9f90fed9426f010421384.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa4506c64e058daa085bcc3d507dacc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1184400b43b5c694ad1",
+    "url": "/static/css/main.40419648.chunk.css"
+  },
+  {
+    "revision": "50eb8c3bff44c5d47436",
+    "url": "/static/js/2.cd07f0ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd07f0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1184400b43b5c694ad1",
+    "url": "/static/js/main.e9fd1253.chunk.js"
+  },
+  {
+    "revision": "1cb751be4be84db33c1c",
+    "url": "/static/js/runtime-main.06ae7549.js"
+  },
+  {
+    "revision": "291fb8b23dbd8e3c0c676eb2c776d5f7",
+    "url": "/static/media/apt.291fb8b2.png"
+  },
+  {
+    "revision": "dd076f3caf8260ccfcbfefd3f31b3957",
+    "url": "/static/media/jlo.dd076f3c.png"
+  },
+  {
+    "revision": "307b90ac5819eb0395f50d3a2cab8797",
+    "url": "/static/media/leader.307b90ac.png"
+  },
+  {
+    "revision": "6aea8e7db0e44e1378d9c1877914ca8c",
+    "url": "/static/media/msk.6aea8e7d.png"
+  }
+]);
